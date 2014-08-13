d206d1a82dc64a50244b415d8911ddf884a64ce7
tiensonqin <tiansheng.qin@upai.com>
2014-08-13 10:44:48 +0800

    remove potemkin

--- src/curator/core.clj
+++
@@ -1,9 +0,0 @@
-(ns curator.core
-  (:require [potemkin.namespaces :refer [import-vars]]))
-
-(import-vars [curator.framework framework]
-             [curator.discovery discovery services provider cache
-              note-error instance instances register unregister update]
-             [curator.leader leader-selector interrupt-leadership
-              leader? leader participants]
-             [curator.path rmr mkdirs])
