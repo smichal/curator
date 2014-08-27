44d9bffbf0613a4ce1aa205cd6cb421ea90a15f2
Michał Stachurski <mstachurski@gmail.com>
2014-08-27 15:08:48 +0200

    name of fork in project.clj

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
-(defproject tiensonqin/curator "0.0.6"
+(defproject smichal/curator "0.0.6"
   :description "Clojurified Apache Curator"
-  :url "https://github.com/tiensonqin/curator"
+  :url "https://github.com/smichal/curator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.apache.curator/curator-recipes "2.4.2"]
                  [org.apache.curator/curator-framework "2.4.2"]
                  [org.apache.curator/curator-x-discovery "2.4.2"]]
-  :signing {:gpg-key "507DEB0B"}
   :profiles {:dev {:dependencies [[org.slf4j/log4j-over-slf4j "1.6.4"]
                                   [org.slf4j/slf4j-simple "1.6.4"]]
                    :exclusions [org.slf4j/slf4j-log4j12]}}
   :scm {:name "git"
-        :url "https://github.com/tiensonqin/curator"})
+        :url "https://github.com/smichal/curator"})
